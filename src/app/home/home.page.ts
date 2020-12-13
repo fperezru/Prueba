02dffa561105eb9b81342b5ffd8d81219f45eba1
franperu <55334114+franperu@users.noreply.github.com>
2020-12-13 13:06:36 +0100

    Añadida lista estática y búsqueda desde searchbar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,23 @@ export class HomePage {
 
   constructor() {}
 
+  public searchbar: any;
+  public items: any;
+
+  ngOnInit() {
+    this.searchbar = document.querySelector('ion-searchbar');
+    this.searchbar.addEventListener('ionInput', this.handleInput);
+  }
+
+  handleInput(event) {
+    this.items = Array.from(document.querySelector('ion-list').children);
+    const query = event.target.value.toLowerCase();
+
+    requestAnimationFrame(() => {
+        this.items.forEach(item => {
+        const shouldShow = item.textContent.toLowerCase().indexOf(query) > -1;
+        item.style.display = shouldShow ? 'block' : 'none';
+      });
+    });
+  }
 }
